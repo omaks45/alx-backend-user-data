ef0f4d822a76698711f79e1c0f1a3cb8cf1e2fea
omaks45 <danielgozie5@gmail.com>
2023-05-30 15:38:01 +0000

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Handling personal data
+"""
+import logger
+from typing import List
+
+
+PIIS_FIELD = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """
+    Replaces vital information in a message  with a redacted
+    value based on the list of field
+    Args:
+        fields: list of fields to redact
+        redaction: the value to use for redaction
+        message: the string message to filter
+        separator: the separator to use between fields
+    """
+    for x in fields:
+        message = re.sub(f'{x}=.*?{separator}',
+                         f'{x}={redaction}{separator}', message)
+
+    return message
