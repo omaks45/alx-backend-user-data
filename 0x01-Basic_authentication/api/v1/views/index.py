50f9c69493b297181e2ac4fd18d2763129a2c4e9
omaks45 <danielgozie5@gmail.com>
2023-05-30 22:47:16 +0000

    gone

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,7 +25,7 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('unauthorized/', methods=['GET'], strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """
     Route: GET /api/v1/unauthorized
