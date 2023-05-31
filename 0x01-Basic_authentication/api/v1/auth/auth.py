e7803d0a81c6d76960cf402c1367af247c730945
omaks45 <danielgozie5@gmail.com>
2023-05-31 22:05:40 +0000

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,6 +17,21 @@ class Auth:
         Returns:
             bool: _description_
         """
+        if path is None:
+            return True
+        if excluded_paths is None or excluded_paths == []
+            return True
+        if path is in excluded_paths:
+            return False
+        for exc_path in excluded_paths:
+            if exc_path.startswith(path):
+                return False
+            elif path.startswith(exc_path):
+                return False
+            elif exc_path[-1] == "*":
+                if path.startswith(exc_path[:-1]):
+                    return False
+            return True
 
     def authorization_header(self, request=None) -> str:
         """_summary_
