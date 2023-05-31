85c9b5ae08321d50d0dfadd14555a3741f15c623
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-05-31 23:12:47 +0100

    auth.py

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,21 +17,22 @@ class Auth:
         Returns:
             bool: _description_
         """
-        if path is None:
-            return True
-        if excluded_paths is None or excluded_paths == []
-            return True
-        if path is in excluded_paths:
-            return False
-        for exc_path in excluded_paths:
-            if exc_path.startswith(path):
+        if excluded_paths is None or excluded_paths == []:
+           return True
+
+        if path in excluded_paths:
+           return False
+
+        for excluded_path in excluded_paths:
+           if excluded_path.startswith(path):
                 return False
-            elif path.startswith(exc_path):
+           elif path.startswith(excluded_path):
                 return False
-            elif exc_path[-1] == "*":
-                if path.startswith(exc_path[:-1]):
+           elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
                     return False
-            return True
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """_summary_
