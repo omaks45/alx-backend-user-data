96801e46497c76cdfa827e0906b94498f5958e6c
omaks45 <danielgozie5@gmail.com>
2023-06-30 14:41:58 +0000

    correct

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,14 +1,31 @@
 #!/usr/bin/env python3
-
-from .auth import Auth
-
 """
 Basic Authentication Api module
 """
+
+from typing import TypeVar
+from api.v1.auth.auth import Auth
+import base64
+
+from models.user import User
+
+
 class BasicAuth(Auth):
+    """_summary_
     """
-    basic authentication class
-    """
-
-
-
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """
+        Args:
+                 authorization_header (str): _description_
+        Returns:
+                 str: _description_
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startwith('Basic'):
+            Heads = authorization_header.split('')[-1]
+            return Heads
