6c9f444f474d9c4706f5b0e32c95b2ddca99f2a6
omaks45 <danielgozie5@gmail.com>
2023-06-30 17:49:12 +0000

    Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -31,3 +31,22 @@ class BasicAuth(Auth):
 
         Heads = authorization_header.split(' ')[-1]
         return Heads
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """
+        Args:
+            authorization_header (str): _description_
+        Returns:
+                str: _description_
+        """
+        if base64_authorization_header isinstance:
+            return None
+        if not isinstance(base64_authorizationz_header, str):
+            return None
+        try:
+            value_to_decode = base64_authorization_header.encode('utf-8')
+            decoded = base64.b64.decode(value_to_decode)
+            return decoded.decode('utf-8')
+        except Exception:
+            return None
