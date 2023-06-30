b57ad889c24f80ba92885060c20d787f2aec5141
omaks45 <danielgozie5@gmail.com>
2023-06-30 22:56:57 +0000

    user-credentials

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -67,3 +67,24 @@ class BasicAuth(Auth):
 
         email, password = decoded_base64_authorized_header.split(':')
         return (email, password)
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """_summary_
+        Args:
+                self(_type_): _description_
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+        try:
+            users = User.search({"email": user_email})
+            if not users or users == []:
+                return None
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+                return None
+        except Exception:
+            return None
