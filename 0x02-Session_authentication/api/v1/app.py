1e4062cded4483fddccb580bf6a16b8cd63c9835
omaks45 <danielgozie5@gmail.com>
2023-07-05 21:42:21 +0000

    session

---
+++ 0x02-Session_authentication/api/v1/app.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+Route module for the API
+"""
+from os import getenv
+from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
+from flask_cors import (CORS, cross_origin)
+import os
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+iauth = None
+
+AUTH_TYPE = os.getenv('AUTH_TYPE')
+# checking for auth_type
+if AUTH_TYPE == 'basic_auth':
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+else:
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """_summary_
+    Returns:
+        _type_: _description_
+    """
+    if auth is None:
+        pass
+    else:
+        excluded_list = ['/api/v1/status/',
+                         '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+        if auth.require_auth(request.path, excluded_list):
+            if auth.authorization_header(request) is None:
+                abort(401, description="Unauthorized")
+            if auth.current_user(request) is None:
+                abort(403, description='Forbidden')
+    request.current_user = auth.current_user
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler
+    """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """_summary_
+    Args:
+        error (_type_): _description_
+    Returns:
+        str: _description_
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbiden(error) -> str:
+    """_summary_
+    Args:
+        error (_type_): _description_
+    Returns:
+        str: _description_
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
+if __name__ == "__main__":
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+an auth module
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """_summary_
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """_summary_
+        Args:
+                path (str): _description_
+                excluded_paths (List[str]): _description_
+        Returns:
+                bool: _description_
+        """
+        if path is None:
+            return True
+
+        if excluded_paths is None or excluded_paths == []:
+            return True
+
+        if path in excluded_paths:
+            return False
+
+        for excluded_path in excluded_paths:
+            if excluded_path.startswith(path):
+                return False
+            elif path.startswith(excluded_path):
+                return False
+            elif excluded_path[-1] == "*":
+                if path.startswith(excluded_path[:-1]):
+                    return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """_summary_
+        Args:
+            request (_tpye_, optional): _description_.Default to None
+        """
+        if request is None:
+            return None
+
+        head = request.headers.get('Authorization')
+        if head is None:
+            return None
+
+        return head
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """_summary_
+        """
+
+        return None
