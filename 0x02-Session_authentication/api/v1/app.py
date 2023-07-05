1cd45c3ed65c220884ffe46a36ffa22aab67630a
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-07-06 00:34:18 +0100

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -9,21 +9,25 @@ from flask_cors import (CORS, cross_origin)
 import os
 
 
+
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
-AUTH_TYPE = os.getenv('AUTH_TYPE')
-# checking for auth_type
+AUTH_TYPE = os.getenv("AUTH_TYPE")
 
-if getenv('AUTH_TYPE') == 'auth':
+# check the AUTH_TYPE
+if AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-else getenv('AUTH_TYPE') == 'basic_auth':
+elif AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 @app.before_request
 def before_request():
