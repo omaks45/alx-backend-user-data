38f9182eafefffe4af1d5b6cf2bd9af2f14f2be3
omaks45 <danielgozie5@gmail.com>
2023-07-06 00:24:04 +0000

    empty session

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for sessional authentication
+"""
+
+from .auth import Auth
+from models.user import User
+from uuid import uuid4
+
+class SessionAuth(Auth):
+    """_empty session class_
+    """
+
