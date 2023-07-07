1fca3aafd2660be98a5982974ba014c2318f1145
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-07-07 19:35:38 +0100

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -39,16 +39,15 @@ def before_request():
     else:
         setattr(request, "current_user", auth.current_user(request))
         excluded_list = ['/api/v1/status/',
-                         '/api/v1/unauthorized/',
-                         '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+                         '/api/v1/unauthorized/', '/api/v1/forbidden/',
+                         '/api/v1/auth_session/login/']
 
         if auth.require_auth(request.path, excluded_list):
-            if auth.authorization_header(request) is None
-            and auth.session_cookie(request) is None:
+            cookie = auth.session_cookie(request)
+            if auth.authorization_header(request) is None and cookie is None:
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description='Forbidden')
-    request.current_user = auth.current_user
 
 
 @app.errorhandler(404)
