b258127444ea0b7045db1ba17c45443f604f18dc
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-07-10 23:10:08 +0100

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -28,7 +28,14 @@ elif AUTH_TYPE == 'session_auth':
     auth = SessionAuth()
 elif getenv('AUTH_TYPE') == 'session_exp_auth':
     from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()elif getenv('AUTH_TYPE') == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
     auth = SessionExpAuth()
+elif getenv('AUTH_TYPE') == 'session_db_auth':
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+
+
 
 
 @app.before_request
