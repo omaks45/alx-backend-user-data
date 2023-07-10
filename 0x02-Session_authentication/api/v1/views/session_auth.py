582a1593633696b7f85e596bb31974d74d69fa8d
Okoro Omaka <107546452+omaks45@users.noreply.github.com>
2023-07-10 02:14:27 +0100

    session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -30,6 +30,7 @@ def session_auth():
             return resp
     return jsonify({"error": "wrong password"}), 401
 
+
 @app_views.route('/auth_session/logout',
                  methods=['DELETE'], strict_slashes=False)
 def logout():
